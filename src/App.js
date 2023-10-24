6822d30ac1a1c3932c281b3a083646d0f5d0b505
Asrat <aabebe.navy@gmail.com>
2023-10-24 16:12:00 -0700

    reset button

--- src/App.js
+++ src/App.js
@@ -21,41 +21,49 @@ const App = () => {
   console.log("treasure:", treasureLocation)
   console.log("bomb:", bombLocation)
 
-const handleGamePlay = (index) => {
-  // alert(index)
-  let updatedBoard = [...board]
-  if (treasureLocation === index) {
-  updatedBoard[index] = "🥇"
-  setBoard(updatedBoard)
-} else if (bombLocation === index) {
-    updatedBoard[index] = "💥"
-    setBoard(updatedBoard)
-  } else {
-    updatedBoard[index] = "🌴"
-    setBoard(updatedBoard)
+  const handleGamePlay = (index) => {
+    // alert(index)
+    let updatedBoard = [...board]
+    if (treasureLocation === index) {
+      updatedBoard[index] = "🥇"
+      setBoard(updatedBoard)
+    } else if (bombLocation === index) {
+      updatedBoard[index] = "💥"
+      setBoard(updatedBoard)
+    } else {
+      updatedBoard[index] = "🌴"
+      setBoard(updatedBoard)
+    }
   }
-}
 
-
+  const handleResetGame = () => {
+    // Reset the game by generating new random positions for the treasure and bomb
+    setTreasureLocation(Math.floor(Math.random() * board.length));
+    setBombLocation(Math.floor(Math.random() * board.length));
+    setBoard(Array(board.length).fill("?"));
+  }
 
   return (
     <>
       <h1>Treasure Hunt Game</h1>
       <div className="gameboard"> 
-      {board.map((value,index) => {
-      return(
-      <Square 
-      value={value}
-      key={index}
-      index={index}
-      handleGamePlay={handleGamePlay}
-      />
-      )
-  })} 
-  </div>
-    </>
-  
-  
+        {board.map((value, index) => {
+          return (
+            <Square 
+              value={value}
+              key={index}
+              index={index}
+              handleGamePlay={handleGamePlay}
+            />
+          );
+        })} 
+      </div>
+      <div> 
+        <button className="reset" onClick={handleResetGame}>Play Again</button>
+        </div>
+      </>
+      
+    
   )
 }
 
---
+++ src/app.md
@@ -0,0 +1,63 @@
+import React, { useState } from "react"
+import "./App.css"
+import Square from "./components/Square"
+
+const App = () => {
+  const [board, setBoard] = useState([
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?"
+  ])
+
+  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
+  const [bombLocation, setBombLocation] = useState(Math.floor(Math.random() * board.length))
+
+  console.log("treasure:", treasureLocation)
+  console.log("bomb:", bombLocation)
+
+const handleGamePlay = (index) => {
+  // alert(index)
+  let updatedBoard = [...board]
+  if (treasureLocation === index) {
+  updatedBoard[index] = "🥇"
+  setBoard(updatedBoard)
+} else if (bombLocation === index) {
+    updatedBoard[index] = "💥"
+    setBoard(updatedBoard)
+  } else {
+    updatedBoard[index] = "🌴"
+    setBoard(updatedBoard)
+  }
+}
+
+
+
+  return (
+    <>
+      <h1>Treasure Hunt Game</h1>
+      <div className="gameboard"> 
+      {board.map((value,index) => {
+      return(
+      <Square 
+      value={value}
+      key={index}
+      index={index}
+      handleGamePlay={handleGamePlay}
+      />
+      )
+  })} 
+  </div>
+  <button className="rea" />
+    </>
+  
+  
+  )
+}
+
+export default App
